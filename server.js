95d00ac55ac6f0d3c02c44f9148f2fa51f639745
bt <btinoco.dev@gmail.com>
2016-02-04 21:41:29 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+    var path = require('path');
+    var app = require('express')();
+    var http = require('http').Server(app);
+    var io = require('socket.io')(http);
+
+    app.get('/', function (req, res) {
+        res.sendFile(path.resolve('index.html'));
+    });
+
+    io.on('connection', function (socket) {
+        console.log('a user connected');
+    });
+
+    http.listen(3000, function () {
+        console.log('listening on *:3000');
+    });
+
+})();
